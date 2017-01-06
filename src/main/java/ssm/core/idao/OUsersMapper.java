7f22db8f69cfc65b26404cfa46f8104d41d3800f
yuxJiang <1724554570@qq.com>
2017-01-06 19:18:08 +0800

    add file

---
+++ src/main/java/ssm/core/idao/OUsersMapper.java
@@ -0,0 +1,18 @@
+package ssm.core.idao;
+
+import ssm.core.entity.OUsers;
+
+public interface OUsersMapper {
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OUsers record);
+
+    int insertSelective(OUsers record);
+
+    OUsers selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OUsers record);
+
+    int updateByPrimaryKey(OUsers record);
+}
---
+++ src/main/java/ssm/core/idao/Tests.java
@@ -0,0 +1,5 @@
+package ssm.core.idao;
+
+public class Tests {
+
+}
