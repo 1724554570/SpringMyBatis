8fc42ec429b5de22703b331f4b02573ecd53982b
yuxJiang <1724554570@qq.com>
2017-01-10 10:42:32 +0800

    remove file

--- src/main/java/ssm/core/controller/OUserController.java
+++ src/main/java/ssm/core/controller/OUserController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ssm.core.entity.OUsers;
+import ssm.core.format.ResultCode;
 import ssm.core.format.ResultMessage;
 import ssm.core.format.Sha1;
 import ssm.core.idao.OUsersMapper;
@@ -20,6 +22,8 @@ import ssm.core.idao.OUsersMapper;
 public class OUserController {
 	@Resource
 	private OUsersMapper mapper;
+
+	// private OUsers oUsers;
 
 	@RequestMapping("/login")
 	@ResponseBody
@@ -33,11 +37,23 @@ public class OUserController {
 		maps.put("name", name);
 		maps.put("pass", pass);
 		try {
-			pass =  Sha1.SHA1(maps);
+			pass = Sha1.SHA1(maps);
 		} catch (DigestException e) {
 			e.printStackTrace();
 		}
-		System.out.println(pass+"::===");
+		OUsers oUsers = new OUsers();
+		oUsers.setUsername(name);
+		oUsers.setUserpass(pass);
+		oUsers = mapper.selectLogin(oUsers);
+		if (oUsers != null) {
+			rm.setData(oUsers);
+			rm.setCode(ResultCode.SUCCESS);
+			rm.setMessage("操作成功。");
+		} else {
+			rm.setCode(ResultCode.EXCP);
+			rm.setMessage("操作失败。");
+		}
+		System.out.println(pass + "::===");
 		return rm;
 	}
 
