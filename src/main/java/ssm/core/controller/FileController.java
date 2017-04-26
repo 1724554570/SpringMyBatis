b3deb8c6d1cfc10b8496f3301ceb82b6533b0d5e
yuxJiang <1724554570@qq.com>
2017-04-26 15:36:04 +0800

    add jsonp

--- src/main/java/ssm/core/controller/FileController.java
+++ src/main/java/ssm/core/controller/FileController.java
@@ -1,9 +1,14 @@
 package ssm.core.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONObject;
+
+import ssm.core.format.ParamToJson;
 import ssm.core.format.ResultCode;
 import ssm.core.format.ResultMessage;
 
@@ -19,5 +24,24 @@ public class FileController {
 		rm.setMessage("查询成功。");
 		rm.setData("上传成功");
 		return rm;
+	}
+
+	@RequestMapping("/getReactFramework")
+	@ResponseBody
+	public String getReactFrameWork7(HttpServletRequest req, String callback) {
+		ResultMessage rm = new ResultMessage();
+		JSONObject jsonObject = ParamToJson.loadElementTojson(req);
+		rm.setCode(ResultCode.SUCCESS);
+		rm.setMessage("查询成功。");
+		rm.setData(jsonObject);
+		String json = JSONObject.toJSONString(rm);
+		String res = null;
+		if (callback != null) {
+			res = callback + "(" + json + ")";
+		} else {
+			res = json;
+		}
+		System.out.println(res);
+		return res;
 	}
 }
