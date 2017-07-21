a659fd5d242d63f9b9123289c37f2df2b2a7e925
yuxJiang <1724554570@qq.com>
2017-07-21 15:01:51 +0800

    build

--- src/test/java/ssm/TestMyBatis.java
+++ src/test/java/ssm/TestMyBatis.java
@@ -16,7 +16,7 @@ import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 // 表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = { "classpath:ssm/core/settings/spring-mybatis.xml" })
+@ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
 public class TestMyBatis {
 	private static Logger logger = Logger.getLogger(TestMyBatis.class);
 	@Resource
